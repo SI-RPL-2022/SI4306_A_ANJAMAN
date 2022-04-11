4848359c6c5d471e39dd1195f257628258439d65
alfasafira <dobela02@gmail.com>
2022-04-11 13:17:49 +0700

    Add: S1-PBI6 [Dashboard Admin] ref #6

---
+++ Frontend/js/demo/chart-2-demo.js
@@ -0,0 +1,27 @@
+
+var xValues = ["Keranjang", "Tas", "Topi", "Pot",];
+var yValues = [55, 49, 44, 24];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+
+new Chart("myChart", {
+  type: "pie",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+    }
+  }
+});
+
