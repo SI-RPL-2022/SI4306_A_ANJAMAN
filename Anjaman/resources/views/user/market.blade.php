09e367cc8928955979b18b64e72c4d75eb11525a
alfasafira <dobela02@gmail.com>
2022-05-13 22:51:23 +0700

    delete unnecessary

--- Anjaman/resources/views/user/market.blade.php
+++ Anjaman/resources/views/user/market.blade.php
@@ -68,20 +68,3 @@ Anjaman | Market
     </section>
     @endif
 @endsection
-
-<script>
-
-    const btn = document.getElementById('btn');
-
-    let index = 0;
-
-    const colors = ['salmon', 'green', 'blue', 'purple'];
-
-    btn.addEventListener('click', function onClick() {
-    btn.style.backgroundColor = colors[index];
-    btn.style.color = 'white';
-
-    index = index >= colors.length - 1 ? 0 : index + 1;
-    });
-    
-</script>
